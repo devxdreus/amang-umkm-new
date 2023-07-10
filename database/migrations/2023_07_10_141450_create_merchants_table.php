2cf1bf8a4b811925fa252aea6ffb5f627139e5b8
devxdreus <andricrafter007@gmail.com>
2023-07-10 22:22:39 +0800

    add migration for merchant, category, and package

---
+++ database/migrations/2023_07_10_141450_create_merchants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('username');
+            $table->string('pic');
+            $table->string('contact');
+            $table->string('email');
+            $table->date('birth');
+            $table->integer('outlet');
+            $table->string('area');
+            $table->bigInteger('gross_income');
+            $table->bigInteger('net_income');
+            $table->string('contract');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+};
