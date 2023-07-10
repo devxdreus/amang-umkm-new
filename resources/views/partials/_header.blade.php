3b6b5e8ff4cd4791e540c675b5d5d8089c4e0ee6
Cammiex <aqilr54@gmail.com>
2023-07-11 00:11:41 +0800

    edit header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -25,12 +25,9 @@
                       <a class="nav-link" href="{{ url('signup') }}" style="font-weight: bold;">Sign Up</a>
                   </li>
               </ul>
-              <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                  <button class="btn btn-outline-success" type="submit">
-                          Search
-                      </button>
-              </form>
+              <div class="d-flex">
+                <a href="{{url('/profile')}}"><img src="https://i.postimg.cc/rsg2HPm6/Group-1.png"></a>
+              </div>
           </div>
       </div>
   </nav>
